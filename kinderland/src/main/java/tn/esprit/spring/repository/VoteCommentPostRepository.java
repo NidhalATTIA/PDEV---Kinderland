de348f36932655d98142863952b18a50b9f7a7e8
FakharElyes <44549958+FakharElyes@users.noreply.github.com>
2020-06-08 12:27:34 +0100

    aa

--- kinderland/src/main/java/tn/esprit/spring/repository/VoteCommentPostRepository.java
+++ kinderland/src/main/java/tn/esprit/spring/repository/VoteCommentPostRepository.java
@@ -1,11 +1,15 @@
 package tn.esprit.spring.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import tn.esprit.spring.entity.Admin;
 import tn.esprit.spring.entity.VoteCommentPost;
 
 
 public interface VoteCommentPostRepository extends CrudRepository<VoteCommentPost, Long>{
+	
+	@Query("SELECT COUNT(r) FROM VoteCommentPost r join User u on r.ups=u.IdUser WHERE u.IdUser=:reporteu AND r.Treated=1")
+    public Long CountReclamationReportedUser (@Param("reporteu")Long iduser2);
 
 }
