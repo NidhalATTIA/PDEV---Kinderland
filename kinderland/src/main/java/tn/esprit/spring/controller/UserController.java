0491f5ad5c6ebd7108b275ee917bf931f54240a0
ATTIA Nidhal <44205085+NidhalATTIA@users.noreply.github.com>
2020-06-10 15:15:49 +0100

    .

--- kinderland/src/main/java/tn/esprit/spring/controller/UserController.java
+++ kinderland/src/main/java/tn/esprit/spring/controller/UserController.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.controller;
 
 import java.util.List;
 
+import org.ocpsoft.rewrite.annotation.Join;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ import tn.esprit.spring.entity.User;
 import tn.esprit.spring.service.UserInterface;
 
 @RestController
+@Join(path = "/", to = "/welcome.jsf")
 public class UserController {
 
 	@Autowired
